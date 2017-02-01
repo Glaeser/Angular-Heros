b656885c1c2796bf7d7e9101950296884bee582e
Sebastian Gläser <glaeser@ad.kwp-info.de>
2017-02-01 11:25:01 +0100

    Http-Connection

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -31,11 +31,19 @@ export class HeroesComponent implements OnInit {
 
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
 
   getHeroes(): void {
 
     this.heroService.getHeroes().then(helden => this.heroes = helden);
-    // this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
@@ -43,6 +51,15 @@ export class HeroesComponent implements OnInit {
 
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
   }
 }
 
